2e45a3aedee36c6524bf5058c59dfa8309ccc9fd
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-11-10 22:27:38 -0300

    criando entidades básicas

---
+++ PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Student
+    {
+        public string Name { get;  set; }
+        public string LastName { get; set; }
+        public string Document { get;  set; }
+        public string Email { get; set; }
+        public string Address { get;  set; }
+    }
+}
