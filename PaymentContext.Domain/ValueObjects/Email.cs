2c91eb967a77abd97c9035a05e250169e79ee19d
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-11-15 16:51:56 -0300

    adicionando notificação no VO email

--- PaymentContext.Domain/ValueObjects/Email.cs
+++ PaymentContext.Domain/ValueObjects/Email.cs
@@ -1,4 +1,5 @@
-﻿using PaymentContext.Shared.ValueObjects;
+﻿using Flunt.Validations;
+using PaymentContext.Shared.ValueObjects;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,10 @@ namespace PaymentContext.Domain.ValueObjects
         public Email(string address)
         {
             Address = address;
+
+            AddNotifications(new Contract()
+                .Requires()
+                .IsEmail(address, "Email.Address", "O e-mail não é válido"));
         }
 
         public string Address { get; private set; }
