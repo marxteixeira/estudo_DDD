8a621a34cc0d18a63ef155285af0ecb98dce87ec
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-11-18 08:25:14 -0300

    criando teste para o command

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,23 @@
+﻿using PaymentContext.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [TestMethod]
+        public void ShoulReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreatePaypalSubscriptionCommand();
+            command.FirstName= "";
+
+            command.Validate();
+            Assert.AreEqual(false, command.Valid);
+        }
+    }
+}
