32fe84ab1fc6cb5af5bcb6e4ec4da8ad3aabb88f
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-11-18 08:14:32 -0300

    implementando método de validação através de Contracts no command

--- PaymentContext.Domain/Commands/CreatePaypalSubscriptionCommand.cs
+++ PaymentContext.Domain/Commands/CreatePaypalSubscriptionCommand.cs
@@ -1,5 +1,8 @@
-﻿using PaymentContext.Domain.Enums;
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using PaymentContext.Domain.Enums;
 using PaymentContext.Domain.ValueObjects;
+using PaymentContext.Shared.Command;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace PaymentContext.Domain.Commands
 {
-    public class CreatePaypalSubscriptionCommand
+    public class CreatePaypalSubscriptionCommand : Notifiable, ICommand
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -33,6 +36,15 @@ namespace PaymentContext.Domain.Commands
         public string State { get; set; }
         public string Country { get; set; }
         public string ZipCode { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "Name.FirstName", "Nome deve conter pelo menos 3 caracteres")
+                .HasMinLen(LastName, 3, "Name.LastName", "Sobrenome deve conter pelo menos 3 caracteres")
+                .HasMaxLen(FirstName, 40, "Name.FirstName", "Nome deve conter no máximo 40 caracteres"));
+        }
     } 
        
         
