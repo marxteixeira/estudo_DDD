069c92ebceda81f0e2ce86ff498af9a082a22c54
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-11-19 17:48:52 -0300

    implementando teste para o subscriptionHandler

--- PaymentContext.Tests/Handlers/SubscriptionHandlerTests.cs
+++ PaymentContext.Tests/Handlers/SubscriptionHandlerTests.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.Handlers;
+using PaymentContext.Tests.Mocks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +16,33 @@ namespace PaymentContext.Tests.Handlers
         [TestMethod]
         public void SubscriptionHandlerTest()
         {
+            var handler = new SubscriptionHandlers(new FakeStudentRespository(), new FakeEmailService());
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "Bruce";
+            command.LastName = "Wayne";
+            command.Document = "99999999999";
+            command.Email = "hello@balta.io2";
+            command.BarCode = "123456789";
+            command.BoletoNumber = "1234654987";
+            command.PaymentNumber = "123121";
+            command.PaidDate = DateTime.Now;
+            command.ExpireDate = DateTime.Now.AddMonths(1);
+            command.Total = 60;
+            command.TotalPaid = 60;
+            command.Payer = "WAYNE CORP";
+            command.PayerDocument = "12345678911";
+            command.PayerDocumentType = EDocumentType.CPF;
+            command.PayerEmail = "batman@dc.com";
+            command.Street = "asdas";
+            command.Number = "asdd";
+            command.Neighborhood = "asdasd";
+            command.City = "as";
+            command.State = "as";
+            command.Country = "as";
+            command.ZipCode = "12345678";
 
+            handler.Handle(command);
+            Assert.AreEqual(false, handler.Valid);
         }
     }
 }
