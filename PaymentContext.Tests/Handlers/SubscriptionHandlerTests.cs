3e8aa1d7d8dba0a4d843cf5c4532b3acb2d43d4e
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-11-19 17:43:37 -0300

    criando repositorio fake para teste do handler

---
+++ PaymentContext.Tests/Handlers/SubscriptionHandlerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Tests.Handlers
+{
+    [TestClass]
+    public class SubscriptionHandlerTests
+    {
+        [TestMethod]
+        public void SubscriptionHandlerTest()
+        {
+
+        }
+    }
+}
---
+++ PaymentContext.Tests/Mocks/FakeStudentRespository.cs
@@ -0,0 +1,34 @@
+﻿using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Tests.Mocks
+{
+    public class FakeStudentRespository : IStudentRepository
+    {
+        public void CreateSubscription(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DocumentExists(string document)
+        {
+            if(document == "99999999999")
+                return true;
+
+            return false;
+        }
+
+        public bool EmailExists(string email)
+        {
+            if (email == "email@email.com")
+                return true;
+
+            return false;
+        }
+    }
+}
