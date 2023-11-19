6159d93fb87769eccfbacdee345ad0306cb6ecea
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-11-19 16:16:37 -0300

    avançando com as etapas de verificação de documento e email

--- PaymentContext.Domain/Handlers/SubscriptionHandlers.cs
+++ PaymentContext.Domain/Handlers/SubscriptionHandlers.cs
@@ -1,6 +1,9 @@
 ﻿using Flunt.Notifications;
 using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
 using PaymentContext.Domain.Repository;
+using PaymentContext.Domain.ValueObjects;
 using PaymentContext.Shared.Command;
 using PaymentContext.Shared.Handlers;
 using System;
@@ -31,6 +34,56 @@ namespace PaymentContext.Domain.Handlers
             }
 
             //verificar se o documento já existe
+            if (_studentRepository.DocumentExists(command.Document))
+            {
+                AddNotification("Document","O documento já existe");
+            }
+
+            //verificar se o e-mail já existe
+            if (_studentRepository.DocumentExists(command.Email))
+            {
+                AddNotification("Email", "O e-mail já existe");
+            }
+
+            // Gerar os VOs
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document, EDocumentType.CPF);
+            var email = new Email(command.Email);
+            var address = new Address(command.Street, command.Number, command.Neighborhood, command.City, command.State, command.Country, command.ZipCode);
+
+            // Gerar as Entidades
+            var student = new Student(name, document, email,address);
+            var subscription = new Subscription(DateTime.Now.AddMonths(1));
+            var payment = new BoletoPayment(
+                command.BarCode,
+                command.BoletoNumber,
+                command.PaidDate,
+                command.ExpireDate,
+                command.Total,
+                command.TotalPaid,
+                command.Payer,
+                new Document(command.PayerDocument, command.PayerDocumentType),
+                address,
+                email
+            );
+
+            // Relacionamentos
+            subscription.AddPayment(payment);
+            student.AddSubscription(subscription);
+
+            // Agrupar as Validações
+            AddNotifications(name, document, email, address, student, subscription, payment);
+
+            // Checar as notificações
+            if (Invalid)
+                return new CommandResult(false, "Não foi possível realizar sua assinatura");
+
+            //salvar as informações
+            _studentRepository.CreateSubscription(student);
+
+
+
+
 
         }
     }
