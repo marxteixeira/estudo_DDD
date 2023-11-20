e05a225a10b844279558fdf52d639568574d73ed
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2023-11-20 11:37:20 -0300

    criando teste para a query

---
+++ PaymentContext.Tests/Queries/StudentQueriesTests.cs
@@ -0,0 +1,26 @@
+﻿using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Tests.Queries
+{
+    [TestClass]
+    public class StudentQueriesTests
+    {
+        private IList<Student> _students;
+
+        [TestMethod]
+        public void ShouldReturnNullWhenDocumentNoExists()
+        {
+            var exp = StudentQueries.GetStudentInfo("12345678911");
+            var studn = _students.AsQueryable().Where(exp).FirstOrDefault();
+
+            Assert.AreEqual(null, studn);
+        }
+
+    }
+}
